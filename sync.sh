09850290e8f0dba515c614e03a80a36dc75023e7
o.chirouze <o.chirouze@criteo.com>
2025-01-20 11:57:03 +0100

    ✨ Make sure directories are properly deleted and improve logging

--- sync.sh
+++ sync.sh
@@ -9,16 +9,11 @@ playlistName=$1
 csvFile="$(dirname "$0")/tmp/$playlistName.csv"
 phoneRoot=${2:-"/storage/sdcard0/syncr"}
 
+# Create the albums csv file
 ts-node export.ts "$playlistName" | sort -u > "$csvFile"
 
 lineCount=$(wc -l < "$csvFile")
-echo "file exported in $csvFile with $lineCount lines"
-
-read -p "Are you sure you want to delete directories in $phoneRoot that are not in $csvFile? [y/N] " confirm
-if [[ $confirm != [yY] ]]; then
-  echo "Operation cancelled."
-  exit 1
-fi
+echo "file exported in $csvFile with $lineCount albums found"
 
 # Create an array of directories from the csvFile
 csvDirs=()
@@ -29,28 +24,56 @@ while IFS= read -r subDir; do
 done < "$tempFile"
 rm "$tempFile"
 
-# List all directories in phoneRoot
-adb shell find "$phoneRoot" -mindepth 2 -maxdepth 2 -type d | while read -r dir; do
-  subDir=${dir#"$phoneRoot"}
-  subDir=${subDir#/}  # Remove leading slash if it exists
+# Create an array of directories in phoneRoot
+phoneDirs=()
+tempFile=$(mktemp)
+adb shell find "$phoneRoot" -mindepth 2 -maxdepth 2 -type d > "$tempFile"
+while IFS= read -r dir; do
+  phoneDirs+=("$dir")
+done < "$tempFile"
+rm "$tempFile"
+
+# List all directories in phoneRoot and find those not in csvDirs
+dirsToDelete=()
+for phoneDir in "${phoneDirs[@]}"; do
   found=false
   for csvDir in "${csvDirs[@]}"; do
-    if [[ "$subDir" == "$csvDir" ]]; then
+    if [[ "$phoneDir" == *"$csvDir" ]]; then
       found=true
+      break
     fi
   done
-  if [[ "$found" == false ]]; then
-    adb shell rm -rf "$dir"
+  if [ "$found" = false ]; then
+    dirsToDelete+=("$phoneDir")
   fi
 done
+
+# Display the list of directories to be deleted
+if [ ${#dirsToDelete[@]} -gt 0 ]; then
+  echo "Directories to be deleted:"
+  for dir in "${dirsToDelete[@]}"; do
+    echo "$dir"
+  done
+
+  read -p "Are you sure you want to delete these directories in $phoneRoot? [y/N] " confirm
+  if [[ $confirm != [yY] ]]; then
+    echo "Operation cancelled."
+    exit 1
+  fi
+
+  # Delete the directories
+  for dir in "${dirsToDelete[@]}"; do
+    echo "Deleting $dir"
+    adb shell rm -rf $(printf %q "$dir")
+  done
+else
+  echo "No directories to delete."
+fi
 
 while IFS=$'\t' read -r -a values; do
   trackDir=${values[0]}
   subDir=${values[1]}
 
-  #echo $trackDir
-  #echo $phoneRoot/$subDir
-
   # exclude hidden MacOS files like ._05 Diaraby.mp3 next to 05 Diaraby.mp3
-  adbsync --exclude "._*" push "$trackDir/" "$phoneRoot/$subDir/"
+  adbsync -q --show-progress --exclude "._*" push "$trackDir/" "$phoneRoot/$subDir/"
 done < "$csvFile"
